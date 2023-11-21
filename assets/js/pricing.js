e24d36b11d527c1cb37bfb021d0b8971aa08909b
Oscar-M-9 <oc6781343@gmail.com>
2023-11-21 08:40:42 -0500

    add: page pricing

---
+++ assets/js/pricing.js
@@ -0,0 +1,40 @@
+
+let intervalo;
+
+$(document).ready(function () {
+    if ($(window).width() > 990) {
+        $(".dropdown").hover(function() {
+            $(this).find(".dropdown-menu").show().addClass("animate__animated animate__zoomIn animate__faster").removeClass("animate__zoomOut");
+        }, function() {
+            $(this).find(".dropdown-menu").hide().addClass("animate__animated animate__zoomOut").removeClass("animate__zoomIn");
+        });
+    } else {
+        $(".dropdown").unbind("hover");
+    }
+
+    const priceAnual = [400, 800]
+    const priceAnualTached = [600, 1200]
+    const priceMensual = [600, 1200]
+    const priceMensualTached = [900, 1800]
+
+    $('#switch-price').change(function() {
+        if ($(this).is(':checked')) {
+            console.log('El switch está activado');
+            $("#price-basico").text(priceMensual[0]);
+            $("#price-basico-old").text('S/ '+ priceMensualTached[0]);
+            $("#price-intermedio").text(priceMensual[1]);
+            $("#price-intermedio-old").text('S/ '+ priceMensualTached[1]);
+            // Realiza acciones si el switch está activado
+        } else {
+            console.log('El switch está desactivado');
+            // Realiza acciones si el switch está desactivado
+            $("#price-basico").text(priceAnual[0]);
+            $("#price-basico-old").text('S/ '+ priceAnualTached[0]);
+            $("#price-intermedio").text(priceAnual[1]);
+            $("#price-intermedio-old").text('S/ '+ priceAnualTached[1]);
+        }
+    });
+
+    
+    
+})
